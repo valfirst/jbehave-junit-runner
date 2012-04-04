6e2a839ea5cb54f8b81e0ec9fa7de4e9e67d6621
Andreas Ebbert-Karroum <andreas@karroum.de>
2012-04-04 09:46:14 +0200

    Trying to reactivate existing unit tests

---
+++ src/test/java/org/jbehave/scenario/finegrained/junit/monitoring/ExampleScenario.java
@@ -0,0 +1,52 @@
+package org.jbehave.scenario.finegrained.junit.monitoring;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jbehave.core.Embeddable;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.i18n.LocalizedKeywords;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.model.ExamplesTableFactory;
+import org.jbehave.core.parsers.RegexStoryParser;
+import org.jbehave.core.steps.ParameterConverters;
+import org.jbehave.core.steps.ParameterConverters.DateConverter;
+import org.jbehave.core.steps.ParameterConverters.ExamplesTableConverter;
+
+//@RunWith(JUnitReportingRunner.class)
+public class ExampleScenario extends JUnitStories {
+
+	public ExampleScenario() {
+	}
+
+	@Override
+	public Configuration configuration() {
+		Class<? extends Embeddable> embeddableClass = this.getClass();
+		// Start from default ParameterConverters instance
+		ParameterConverters parameterConverters = new ParameterConverters();
+		// factory to allow parameter conversion and loading from external
+		// resources (used by StoryParser too)
+		ExamplesTableFactory examplesTableFactory = new ExamplesTableFactory(
+				new LocalizedKeywords(),
+				new LoadFromClasspath(embeddableClass), parameterConverters);
+		// add custom coverters
+		parameterConverters.addConverters(new DateConverter(
+				new SimpleDateFormat("yyyy-MM-dd")),
+				new ExamplesTableConverter(examplesTableFactory));
+		return new MostUsefulConfiguration()
+				.useStoryLoader(new LoadFromClasspath(embeddableClass))
+				.useStoryParser(new RegexStoryParser(examplesTableFactory))
+				.useParameterConverters(parameterConverters);
+	}
+
+	@Override
+	protected List<String> storyPaths() {
+		List<String> stories = new ArrayList<String>();
+		stories.add("org/jbehave/scenario/finegrained/junit/monitoring/example_scenario");
+		return stories;
+	}
+
+}

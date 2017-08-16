a345812f8fe6e80b8626b44eeb9f960842737bb6
Valery Yatsynovich <valfirst@yandex.ru>
2017-08-17 00:27:16 +0300

    Optimize JUnitStringDecorator

--- src/main/java/de/codecentric/jbehave/junit/monitoring/JUnitStringDecorator.java
+++ src/main/java/de/codecentric/jbehave/junit/monitoring/JUnitStringDecorator.java
@@ -5,16 +5,11 @@ package de.codecentric.jbehave.junit.monitoring;
  */
 public final class JUnitStringDecorator
 {
-	private JUnitStringDecorator()
-	{
+	private JUnitStringDecorator() {
 		// Utility class
 	}
 
 	public static String getJunitSafeString(String string) {
-		return replaceLinebreaks(string).replaceAll("[\\(\\)]", "|");
-	}
-
-	private static String replaceLinebreaks(String string) {
-		return string.replaceAll("\r", "\n").replaceAll("\n+", ", ");
+		return string.replaceAll("[\r\n]+", ", ").replaceAll("[()]", "|");
 	}
 }
